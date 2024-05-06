219aaeaa016ea2f0ead13dbe1bfcfcd3846254ab
whosmars <mariosalazar@ciencias.unam.mx>
2024-05-06 14:52:24 -0600

    Ver comentarios sin PostMan

---
+++ backEnd/controllers/comentarioController.py
@@ -0,0 +1,81 @@
+from flask import Blueprint, request, jsonify, session, url_for
+from cryptoUtils import CryptoUtils as crypto
+from itsdangerous import URLSafeTimedSerializer, SignatureExpired, BadTimeSignature
+
+from model import modelComentario as model
+
+comentario_blueprint = Blueprint('comentario', __name__,url_prefix='/comentario')
+
+@comentario_blueprint.route('/crear-comentario', methods=['POST'])
+def crear_comentario():
+    id_producto = request.json['id_producto'] #may it should have a .get() method
+    id_usuario = request.json['id_usuario']
+    comentario = request.json['comentario']
+
+    if (id_producto is None or id_usuario is None or comentario is None):
+        return jsonify({"error": "Datos faltantes para crear el comentario"}), 400
+    
+    try:    
+        nuevo_comentario = model.crear_comentario(id_producto, id_usuario, comentario)
+        return jsonify({
+            'id_comentario': nuevo_comentario.id_comentario,
+            'id_producto': nuevo_comentario.id_producto,
+            'id_usuario': nuevo_comentario.id_usuario,
+            'comentario': nuevo_comentario.comentario   
+        }), 201
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+    
+@comentario_blueprint.route('/ver-comentarios', methods=['GET'])
+def ver_comentarios():
+    comentarios = model.ver_comentarios()
+    if comentarios is None:
+        return jsonify({"error": "No hay comentarios"}), 404
+    return jsonify([{
+        'id_comentario': comentario.id_comentario,
+        'id_producto': comentario.id_producto,
+        'id_usuario': comentario.id_usuario,
+        'comentario': comentario.comentario
+    } for comentario in comentarios]), 200
+
+@comentario_blueprint.route('/ver-comentario/<int:id_comentario>', methods=['GET'])
+def ver_comentario(id_comentario):
+    comentario = model.ver_comentario_por_id(id_comentario)
+    if comentario is None:
+        return jsonify({"error": "No se encontro el comentario"}), 404
+    return jsonify({
+        'id_comentario': comentario.id_comentario,
+        'id_producto': comentario.id_producto,
+        'id_usuario': comentario.id_usuario,
+        'comentario': comentario.comentario
+    }), 200
+
+@comentario_blueprint.route('/actualizar/<int:id_comentario>', methods=['PUT'])
+def actualizar_comentario(id_comentario):
+    nuevo_texto = request.json.get('comentario')
+    if not nuevo_texto:
+        return jsonify({'error': 'Texto del comentario es necesario'}), 400
+    else:
+        comentario = model.actualizar_comentario(id_comentario, nuevo_texto)
+        if comentario:
+            return jsonify({
+                'id_comentario': comentario.id_comentario,
+                'id_producto': comentario.id_producto,
+                'id_usuario': comentario.id_usuario,
+                'comentario': comentario.comentario
+            }), 200
+        else:
+            return jsonify({'error': 'Comentario no encontrado'}), 404
+        
+@comentario_blueprint.route('/eliminar/<int:id_comentario>', methods=['DELETE'])    
+def eliminar_comentario(id_comentario):
+    comentario = model.eliminar_comentario(id_comentario)
+    if comentario:
+        return jsonify({
+            'id_comentario': comentario.id_comentario,
+            'id_producto': comentario.id_producto,
+            'id_usuario': comentario.id_usuario,
+            'comentario': comentario.comentario
+        }), 200
+    else:
+        return jsonify({'error': 'Comentario no encontrado'}), 404
---
+++ backEnd/model/modelComentario.py
@@ -0,0 +1,53 @@
+from flask import request, abort, session
+from flask.json import jsonify
+from cryptoUtils import CryptoUtils as crypto
+
+from alchemyClasses.Comentario import Comentario
+from alchemyClasses import db
+
+def crear_comentario(id_producto, id_usuario, comentario):
+    nuevo_comentario = Comentario(id_producto=id_producto, id_usuario=id_usuario, comentario=comentario)
+    try:
+        db.session.add(nuevo_comentario)
+        db.session.commit()
+        return nuevo_comentario
+    except Exception as e:
+        db.session.rollback() # Rollback the changes on error
+        abort(400, str(e))
+
+def eliminar_comentario(id_comentario):
+    comentario = Comentario.query.filter_by(id_comentario=id_comentario).first()
+    if comentario is None:
+        return None
+    try:
+        db.session.delete(comentario)
+        db.session.commit()
+        return comentario
+    except Exception as e:
+        db.session.rollback() # Rollback the changes on error
+        abort(400, str(e))
+
+def ver_comentarios():
+    comentarios = Comentario.query.all()
+    if comentarios is None:
+        return None
+    return comentarios
+
+def ver_comentario_por_id(id_comentario):
+    comentario = Comentario.query.filter_by(id_comentario=id_comentario).first()
+    if comentario is None:
+        return None
+    return comentario
+
+def actualizar_comentario(id_comentario, comentario):
+    comentario = Comentario.query.filter_by(id_comentario=id_comentario).first()
+    if comentario is None:
+        return None
+    comentario.comentario = comentario
+    try:
+        db.session.commit()
+        return comentario
+    except Exception as e:
+        db.session.rollback() # Rollback the changes on error
+        abort(400, str(e))
+        
