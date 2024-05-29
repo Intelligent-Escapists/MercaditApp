624c4485d76208c497a5c3f57dd156a9dd80d19b
Jorge <jorgealberto@ciencias.unam.mx>
2024-05-29 15:27:29 -0600

    Se agrego el endpoint para buscar producto por nombre

--- backEnd/model/modelProducto.py
+++ backEnd/model/modelProducto.py
@@ -161,3 +161,13 @@ def producto_existe(id_producto):
     except Exception as e:
         print(f"Error: {e}")
         abort(400, str(e))
+
+
+def buscar_producto_por_nombre(nombre_producto):
+    try:
+         productos =db.session.query(Producto.id_producto).filter(Producto.nombre.like(f"%{nombre_producto}%")).all()
+    except Exception as e:
+        print(f"Error: {e}")
+        abort(400, str(e))
+    if productos:
+        return [producto.id_producto for producto in productos]
