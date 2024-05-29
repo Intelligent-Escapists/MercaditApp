72a174226175509eccb8f4f603ed718fd14da579
Rodrigo <rogerxdgta@ciencias.unam.mx>
2024-05-29 11:42:33 -0600

    Detalle usuario

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -9,6 +9,7 @@ import HomePanel from './components/Root/HomePanel';
 import CorreoEnviado from './components/RegisterPanel/CorreoEnviado';
 import CorreoConfirmado from './components/RegisterPanel/CorreoConfirmado';
 import RegistrarProducto from './components/Forms/RegistrarProducto';
+import DetalleUsuario from './components/User/DetalleUsuario';
 import { UserProvider } from './providers/UserProvider';
 
 function App() {
@@ -23,6 +24,7 @@ function App() {
         <Route path="homeCompra" element={< HomeComprador />} />
         <Route path="homeVenta" element={< HomeVendendor />} />
         <Route path="registra-producto" element={< RegistrarProducto />} />
+        <Route path="detalle-usuario" element={< DetalleUsuario />} />
       </Route>
     </>
   ));
